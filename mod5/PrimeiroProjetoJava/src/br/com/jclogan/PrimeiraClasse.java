d47dec27c8c4232df30ff759c65476d68decbed6
Jorge Chediack <jorge.logan@gmail.com>
2024-01-15 20:47:51 -0300

    tarefas modulo 5

---
+++ mod5/PrimeiroProjetoJava/src/br/com/jclogan/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.jclogan;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("By Jedi Logan...");
+
+	}
+
+}
