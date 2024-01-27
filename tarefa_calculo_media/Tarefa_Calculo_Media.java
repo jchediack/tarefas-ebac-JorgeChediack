1b944d413b432d0d3d7fe2957dbf33a0c08a5c8a
Jorge Chediack <jorge.logan@gmail.com>
2024-01-27 17:30:49 -0300

    Tarefa Modulo 8

---
+++ tarefa_calculo_media/Tarefa_Calculo_Media.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tarefa_Calculo_Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota:");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota:");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota:");
+        double nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota:");
+        double nota4 = scanner.nextDouble();
+
+        System.out.println("A média das notas é: " + (nota1 + nota2 + nota3 + nota4) / 4);
+        scanner.close();
+    }
+}
+
+
+
