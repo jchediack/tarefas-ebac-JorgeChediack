2cc139712df9d9802b74e4a5a9586d9a8ac67be9
Jorge Chediack <jorge.logan@gmail.com>
2024-07-29 22:51:18 -0300

    mod37

---
+++ mod37/src/main/java/com/jchediack/ebac/mod37/Mod37Application.java
@@ -0,0 +1,13 @@
+package com.jchediack.ebac.mod37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod37Application.class, args);
+	}
+
+}
