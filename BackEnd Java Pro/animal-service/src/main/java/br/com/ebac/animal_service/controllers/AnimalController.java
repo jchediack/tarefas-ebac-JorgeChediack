1daafad08ebc686501a9239e223103a8c9226774
Jorge Chediack <jorge.logan@gmail.com>
2025-02-05 12:33:47 +0000

    Criado EndPoints POST e GETs de Animais

---
+++ BackEnd Java Pro/animal-service/src/main/java/br/com/ebac/animal_service/controllers/AnimalController.java
@@ -0,0 +1,40 @@
+package br.com.ebac.animal_service.controllers;
+
+import br.com.ebac.animal_service.entidades.Animal;
+import br.com.ebac.animal_service.repositorios.AnimalRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/animais")
+public class AnimalController {
+
+    private AnimalRepository repository;
+
+    public AnimalController(AnimalRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    private List<Animal> findAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    private Animal create(@RequestBody Animal animal){
+        return repository.save(animal);
+    }
+
+    @GetMapping("/not-adopted")
+    private List<Animal> findNotAdopted() {
+        return repository.findNotAdopted();
+    }
+
+    @GetMapping("/adopted")
+    private List<Animal> findAdopted() {
+        return repository.findAdopted();
+    }
+
+}
---
+++ BackEnd Java Pro/animal-service/src/main/java/br/com/ebac/animal_service/repositorios/AnimalRepository.java
@@ -0,0 +1,17 @@
+package br.com.ebac.animal_service.repositorios;
+
+import br.com.ebac.animal_service.entidades.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AnimalRepository extends JpaRepository<Animal, Integer> {
+
+    @Query("SELECT a FROM Animal a WHERE a.dataAdocao IS NULL ORDER BY a.dataEntrada")
+    List<Animal> findNotAdopted();
+
+    @Query("SELECT a FROM Animal a WHERE a.dataAdocao IS NOT NULL")
+    List<Animal> findAdopted();
+
+}
