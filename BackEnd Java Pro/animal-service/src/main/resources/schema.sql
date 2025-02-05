d21d8372810388564bac4a708da51d03dc372123
Jorge Chediack <jorge.logan@gmail.com>
2025-02-05 00:10:24 +0000

    modulo 9 - aula 9.5

---
+++ BackEnd Java Pro/animal-service/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+  CREATE TABLE animal (
+      id INT AUTO_INCREMENT NOT NULL,
+      nome_provisorio VARCHAR(255) NOT NULL,
+      idade_estimada INT NOT NULL,
+      raca VARCHAR(255) NOT NULL,
+      data_entrada DATE NOT NULL,
+      data_adocao DATE,
+      condicoes_chegada TEXT NOT NULL,
+      nome_recebedor VARCHAR(255) NOT NULL,
+      data_obito DATE,
+      porte VARCHAR(50) NOT NULL,
+      CONSTRAINT pk_animal PRIMARY KEY (id)
+  );
